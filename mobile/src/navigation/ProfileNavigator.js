fa89af86c66fbcd98a2140c2505c9e7a2a1998f0
Sharada Gurrala <sharada@magzest.in>
2026-06-30 10:09:56 +0530

    Add work info screen and update profile navigation

--- mobile/src/navigation/ProfileNavigator.js
+++ mobile/src/navigation/ProfileNavigator.js
@@ -4,6 +4,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "../screens/employee/ProfileScreen";
 import PersonalInfoScreen from "../screens/employee/PersonalInfoScreen";
 import WorkInfoScreen from "../screens/employee/WorkInfoScreen";
+import ContactScreen from "../screens/employee/ContactScreen";
+import EmergencyContactScreen from "../screens/employee/EmergencyContactScreen";
+import EducationScreen from "../screens/employee/EducationScreen";
+import ExperienceScreen from "../screens/employee/ExperienceScreen";
+import DocumentsScreen from "../screens/employee/DocumentsScreen";
+import BankDetailsScreen from "../screens/employee/BankDetailsScreen";
+import SecurityScreen from "../screens/employee/SecurityScreen";
+import SettingsScreen from "../screens/employee/SettingsScreen";
 
 const Stack = createStackNavigator();
 
@@ -19,9 +27,50 @@ export default function ProfileNavigator() {
         name="PersonalInfo"
         component={PersonalInfoScreen}
       />
-       <Stack.Screen
+
+      <Stack.Screen
         name="WorkInfo"
         component={WorkInfoScreen}
+      />
+
+      <Stack.Screen
+        name="Contact"
+        component={ContactScreen}
+      />
+
+      <Stack.Screen
+        name="EmergencyContact"
+        component={EmergencyContactScreen}
+      />
+
+      <Stack.Screen
+        name="Education"
+        component={EducationScreen}
+      />
+
+      <Stack.Screen
+        name="Experience"
+        component={ExperienceScreen}
+      />
+
+      <Stack.Screen
+        name="Documents"
+        component={DocumentsScreen}
+      />
+
+      <Stack.Screen
+        name="BankDetails"
+        component={BankDetailsScreen}
+      />
+
+      <Stack.Screen
+        name="Security"
+        component={SecurityScreen}
+      />
+
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
       />
     </Stack.Navigator>
   );
